924922ab4d707f4d352da9f60042ac148dbedae2
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2021-11-19 15:26:09 +0100

    adapt arguments to fit param_t structure

--- buffer.c
+++ buffer.c
@@ -6,18 +6,27 @@
 
 #define SIZE 8
 
+typedef struct param{
+    int *number, *index, *counter;
+    int *buffer;
+    sem_t *empty, *full;
+    pthread_mutex_t *mutex_buf, *mutex_pc;
+}param_t;
+
 void *producer(void* voidArg){
-    usleep(10);
-    char** args = (char**)voidArg;
-    int *producer_number = (int*)args[0];
-    printf("%d\n", *producer_number);
+    //TODO
+    param_t *arg = (param_t*) voidArg;
+    printf("Number: %d, Counter: %d, \n", *arg->number, *arg->counter);
     return NULL;
 }
 
 void *consumer(void* voidArg){
-    char** args = (char**)voidArg;
-    int *consumer_number = (int*)args[0];
-    printf("%d\n", *consumer_number);
+    //TODO
+    param_t *arg = (param_t*) voidArg;
+    pthread_mutex_lock(arg->mutex_pc);
+    *arg->counter = *arg->counter+1;
+    printf("Number: %d, Counter: %d, \n", *arg->number, *arg->counter);
+    pthread_mutex_unlock(arg->mutex_pc);
     return NULL;
 }
 
@@ -26,17 +35,13 @@ int main(int argc, char* argv[]){
         printf("You have to pass two arguments at the program.\n First one is the number of producers (int)\n Second one is number of consumers (int)\n");
         return EXIT_FAILURE;
     }
-    // malloc of all shared variables
-    int *producer_number = (int*)malloc(sizeof(int));
-    if(!producer_number) return EXIT_FAILURE;
-    int *consumer_number = (int*)malloc(sizeof(int));
-    if(!consumer_number) return EXIT_FAILURE;
-    // forced to convert the last one first because the endptr of strtol consume it.
-    // to avoid this consummation we can reduce the address by one and then don't
-    // the next argument
-    *producer_number = (int)strtol(argv[1], &argv[2] - 1, 10);
-    *consumer_number = (int)strtol(argv[2], &argv[3] - 1, 10);
-    printf("P: %d -- C: %d\n", *producer_number, *consumer_number);// TODO rm
+
+    // shared variables
+    int buffer[SIZE];
+    int producer_number = (int)strtol(argv[1], &argv[2] - 1, 10);
+    int consumer_number = (int)strtol(argv[2], &argv[3] - 1, 10);
+    int produced, consumed, cons_index, prod_index = 0;
+    printf("P: %d -- C: %d\n", producer_number, consumer_number);// TODO rm
 
     // semaphore and mutexes + init mutexes
     sem_t empty, full;
@@ -47,71 +52,52 @@ int main(int argc, char* argv[]){
     pthread_mutex_init(&producer_m, NULL);
     pthread_mutex_init(&consumer_m, NULL);
 
-    // definition of the shared variables remaining
-    int buffer[SIZE];
-    int *producer_index = malloc(sizeof(int));
-    if(!producer_index) return EXIT_FAILURE;
-    int *consumer_index = malloc(sizeof(int));
-    if(!consumer_index) return EXIT_FAILURE;
-    *producer_index = 0, *consumer_index = 0;
-    int *produced = malloc(sizeof(int));
-    if(!produced) return EXIT_FAILURE;
-    int *consumed = malloc(sizeof(int));
-    if(!consumed) return EXIT_FAILURE;
-    *produced = 0, *consumed = 0;
-
     // threads
-    pthread_t consumers[*consumer_number];
-    pthread_t producers[*producer_number];
+    pthread_t consumers[consumer_number];
+    pthread_t producers[producer_number];
 
-    void** prod_args = malloc(8*sizeof(void*));
-    prod_args[0] = producer_number;
-    prod_args[1] = produced;
-    prod_args[2] = &producer_m;
-    prod_args[3] = &buffer_m;
-    prod_args[4] = &buffer;
-    prod_args[5] = &empty;
-    prod_args[6] = &full;
-    prod_args[7] = producer_index;
-    for(int i = 0; i < *producer_number; i++){
-        pthread_create(&producers[i], NULL, &producer, (void*)prod_args);
+    // init producer param and threads
+    param_t *prod_param = malloc(sizeof(param_t));
+    if(!prod_param) return EXIT_FAILURE;
+    prod_param->buffer = &buffer[0];
+    prod_param->number = &producer_number;
+    prod_param->counter = &produced;
+    prod_param->index = &prod_index;
+    prod_param->empty = &empty;
+    prod_param->full = &full;
+    prod_param->mutex_buf = &buffer_m;
+    prod_param->mutex_pc = &producer_m;
+    for(int i = 0; i < producer_number; i++){
+        pthread_create(&producers[i], NULL, &producer, (void*)prod_param);
     }
 
-    void** cons_args = malloc(8*sizeof(void*));
-    cons_args[0] = consumer_number;
-    cons_args[1] = consumed;
-    cons_args[2] = &consumer_m;
-    cons_args[3] = &buffer_m;
-    cons_args[4] = &buffer;
-    cons_args[5] = &empty;
-    cons_args[6] = &full;
-    cons_args[7] = consumer_index;
-    for(int i = 0; i < *consumer_number; i++){
-        pthread_create(&consumers[i], NULL, &consumer, (void*)cons_args);
+    // init consumer param and threads
+    param_t *cons_param = malloc(sizeof(param_t));
+    if(!cons_param) return EXIT_FAILURE;
+    cons_param->buffer = &buffer[0];
+    cons_param->number = &consumer_number;
+    cons_param->counter = &consumed;
+    cons_param->index = &cons_index;
+    cons_param->empty = &empty;
+    cons_param->full = &full;
+    cons_param->mutex_buf = &buffer_m;
+    cons_param->mutex_pc = &consumer_m;
+    for(int i = 0; i < consumer_number; i++){
+        pthread_create(&consumers[i], NULL, &consumer, (void*)cons_param);
     }
 
     // wait for all threads end
-    for(int i = 0; i < *consumer_number; i++){
-        pthread_join(producers[i], NULL);
-    }
+    for(int i = 0; i < producer_number; i++) pthread_join(producers[i], NULL);
+    for(int i = 0; i < consumer_number; i++) pthread_join(consumers[i], NULL);
 
-    for(int i = 0; i < *consumer_number; i++){
-        pthread_join(consumers[i], NULL);
-    }
-
+    // garbage
     pthread_mutex_destroy(&buffer_m);
     pthread_mutex_destroy(&producer_m);
     pthread_mutex_destroy(&consumer_m);
     sem_destroy(&empty);
     sem_destroy(&full);
     // free all malloced variables
-    free(producer_number);
-    free(consumer_number);
-    free(consumer_index);
-    free(producer_index);
-    free(produced);
-    free(consumed);
-    free(cons_args);
-    free(prod_args);
+    free(prod_param);
+    free(cons_param);
     return EXIT_SUCCESS;
 }

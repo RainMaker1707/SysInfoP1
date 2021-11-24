e8aad2871e9f43db10bb2465b44387b7aa5942a4
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2021-11-24 13:18:28 +0100

    add struct for parameter in philosophers

--- philosophers.c
+++ philosophers.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-
+#include "philosophers.h"
 #define ITERATIONS 100000
 
 void eat(){
@@ -13,30 +10,23 @@ void think(){
 }
 
 void *party(void* voidArg){
-    // get and cast arg
-    void** args = (void**)voidArg;
-    int *id = (int*)args[0];
-    int *N = (int*)args[1];
-    pthread_mutex_t *chopsticks[*N];
-    for(int i = 0; i < *N; i++){
-        chopsticks[i] = (pthread_mutex_t *)args[2+i];
-    }
-
+    param_t *args = (param_t*) voidArg;
     // syllabus code with no deadlock
     for(int i = 0; i < ITERATIONS; i++){
-        int left = *id;
-        int right = (*id+1) % *N;
+        int left = args->id;
+        int right = (args->id+1) % args->N;
         think();
         if(left < right){
-            pthread_mutex_lock(chopsticks[left]);
-            pthread_mutex_lock(chopsticks[right]);
+            pthread_mutex_lock(args->chopsticks + left);
+            pthread_mutex_lock(args->chopsticks + right);
         }else{
-            pthread_mutex_lock(chopsticks[right]);
-            pthread_mutex_lock(chopsticks[left]);
+            pthread_mutex_lock(args->chopsticks + right);
+            pthread_mutex_lock(args->chopsticks + left);
         }
         eat();
-        pthread_mutex_unlock(chopsticks[left]);
-        pthread_mutex_unlock(chopsticks[right]);
+        // if(i%10000 == 0) printf("id: %d, round: %d\n", args->id, i); // TODO rm debug line
+        pthread_mutex_unlock(args->chopsticks + left);
+        pthread_mutex_unlock(args->chopsticks + right);
     }
     // free of arguments
     free(args);
@@ -60,34 +50,19 @@ int main(int argc, char* argv[]) {
         pthread_t thread[philosophers_number];
 
         // malloc all shared variables
-        int *pn = malloc(sizeof(int));
-        if(!pn) return EXIT_FAILURE;
-        int *ids = malloc((sizeof(int)*philosophers_number));
-        if(!ids) return EXIT_FAILURE;
-        //init variables: pn, ids array and mutex
-        *pn = philosophers_number;
-        for (int i=0; i < philosophers_number; i++) {
-            ids[i] = i;
-            pthread_mutex_init(&chopsticks[i], NULL);
-        }
+        for (int i=0; i < philosophers_number; i++) pthread_mutex_init(&chopsticks[i], NULL);
 
         for(int i = 0; i < philosophers_number; i++){
-            // make tab of arguments addresses (freed in each thread end)
-            void **args = malloc((2 + philosophers_number) * sizeof(void*));
-            if(!args) return EXIT_FAILURE;
-            args[0] = &ids[i];
-            args[1] = pn;
-            for(int j = 0; j < philosophers_number; j++) args[2+j] = &(chopsticks[j]);
-            pthread_create(&thread[i], NULL, party, (void*)args);
+            param_t *arg = (param_t*)malloc(sizeof(param_t));
+            arg->id = i;
+            arg->N = philosophers_number;
+            arg->chopsticks = &chopsticks[0];
+            pthread_create(&thread[i], NULL, party, (void*)arg);
         }
 
         // wait for all thread to end and then destroy the mutex
         for(int i = 0; i< philosophers_number; i++) pthread_join(thread[i], NULL);
         for(int i = 0; i< philosophers_number; i++) pthread_mutex_destroy(&chopsticks[i]);
-
-        // free malloced variables
-        free(pn);
-        free(ids);
     }
     return EXIT_SUCCESS;
 }

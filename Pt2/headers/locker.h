419760ffe80ef800ffbbd4603db3419cdcecc926
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2021-12-05 19:31:19 +0100

    init P2

--- Pt2/headers/locker.h
+++ Pt2/headers/locker.h
@@ -1,4 +1,13 @@
 #ifndef SYSINFOP1_LOCKER_H
 #define SYSINFOP1_LOCKER_H
 
+typedef struct locker{
+    int state;
+}lock_t;
+
+void lock_init(lock_t *mutex);
+void lock(lock_t *mutex);
+void unlock(lock_t *mutex);
+void test_and_test_and_set_lock(lock_t *mutex);
+
 #endif //SYSINFOP1_LOCKER_H
--- Pt2/headers/semaphore.h
+++ Pt2/headers/semaphore.h
@@ -1,4 +1,17 @@
 #ifndef SYSINFOP1_SEMAPHORE_H
 #define SYSINFOP1_SEMAPHORE_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "../headers/locker.h"
+
+typedef struct semaphore{
+    unsigned int value;
+    lock_t mutex;
+}sem_t2;
+
+void sem_init_(sem_t2 *sem, unsigned int value);
+void sem_wait_(sem_t2 *sem);
+void sem_post_(sem_t2 *sem);
+
 #endif //SYSINFOP1_SEMAPHORE_H

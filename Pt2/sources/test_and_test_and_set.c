5ca7fb2c7c417b87e08a3ed50d63d4e16bd087bf
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2021-12-07 17:10:27 +0100

    patched tas and ttas?

--- Pt2/sources/test_and_test_and_set.c
+++ Pt2/sources/test_and_test_and_set.c
@@ -20,10 +20,12 @@ void *threading(void *arg){
 
 
 int main(int argc, char *argv[]){
-    if(argc != 1) return EXIT_FAILURE;
+    if(argc != 2) return EXIT_FAILURE;
+
     int thread_number = (int)strtol(argv[1], &argv[2]-1, 10);
     pthread_t threads[thread_number];
     int round = TOTAL/thread_number;
+
     lock_v = (lock_t*)malloc(sizeof(lock_t));
     if(!lock_v) return EXIT_FAILURE;
     lock_init(lock_v);
@@ -32,7 +34,9 @@ int main(int argc, char *argv[]){
         if(i == thread_number-1) round += TOTAL % thread_number;
         pthread_create(&threads[i], NULL, &threading, (void *)&round);
     }
+
     for(int i=0; i < thread_number; i++) pthread_join(threads[i], NULL);
+    free(lock_v);
 }
 
 
